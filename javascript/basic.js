6f6b4a7d48b52ae6d64ebb871b9c8a0ad7aacd26
lukman2458 <lukmanh2458@gmail.com>
2025-07-01 11:03:56 +0530

    javascript_basics

---
+++ javascript/basic.js
@@ -0,0 +1,24 @@
+var fruit = "Apple"
+var count = 2 
+var price = 50
+var total = count*price
+console.log(fruit)
+console.log(total) 
+// Task 1 : variables
+// ==================
+function add(a,b)
+{ console.log(a+b)}
+add(5,10)
+//Task 2 : Functions and parameters
+// =================
+function ret(a,b)
+{return(a+b)}
+var c = ret(20,60)
+console.log("return value is : "+c)
+//Task 3 : return keyword
+// =====================
+if (c == 80)
+{console.log("its true")}
+else{console.log("false")}
+//Task 4 : if else 
+// ===============
